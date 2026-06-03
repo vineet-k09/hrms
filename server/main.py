8406000dd00257686270bf3ca66b03e77dbfe828
Vineet Kushwaha <vineetkushwaha6325@gmail.com>
2026-06-03 22:16:08 +0530

    merge cleanup

--- server/main.py
+++ server/main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
 import uvicorn
 
-from src.routes.auth import router as auth_router
 from src.middleware.cors import setup_middlewares
-#
+# from fastapi.middleware.cors import CORSMiddleware
+
+
+# Common routes
+from src.routes.auth import router as auth_routes_router
 from src.routes.admin import router as admin_routes_router
 from src.routes.employee import router as employee_routes_router
 from src.routes.candidate import router as candidate_routes_router
 from src.routes.hr import router as hr_routes_router
-# from fastapi.middleware.cors import CORSMiddleware
+
+# Module Routes
 from src.modules.attendance.router import router as attendance_router
 from src.modules.employee.router import router as employee_router
 from src.modules.leave.router import router as leave_router
@@ -17,14 +21,17 @@ app = FastAPI()
 
 setup_middlewares(app)
 
-app.include_router(auth_router)
-
+# Common routes
 app.include_router(auth_routes_router)
 app.include_router(admin_routes_router)
 app.include_router(employee_routes_router)
 app.include_router(candidate_routes_router)
 app.include_router(hr_routes_router)
 
+# Module routes
+app.include_router(employee_router)
+app.include_router(attendance_router)
+app.include_router(leave_router)
 
 # Handle CORS
 
@@ -37,10 +44,6 @@ app.include_router(hr_routes_router)
 #     allow_methods=["*"],
 #     allow_headers=["*"],
 # )
-
-app.include_router(employee_router)
-app.include_router(attendance_router)
-app.include_router(leave_router)
 
 @app.get("/")
 def root():
